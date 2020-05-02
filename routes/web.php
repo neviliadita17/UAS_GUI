d09ca98e771e9934406dd0918311a89cc1714af4
FZ-Q <50388521+FZ-Q@users.noreply.github.com>
2020-05-02 07:31:02 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pegawai/login', 'LoginPegC@login');
+
+// Akun Pasien
+Route::get('pegawai/akun-pasien', 'AkunPasienC@akunPasien');
+Route::get('pegawai/akun-pasien/data-api', 'AkunPasienC@dataAPI');
+
+// Register Pasien from Pegawai
+Route::get('pegawai/akun-pasien/form-register', 'RegPasienC@formRegister');
+Route::post('pegawai/akun-pasien/form-register/action', 'RegPasienC@registerAction');
