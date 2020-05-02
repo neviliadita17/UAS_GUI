7827e8dfd786fc3aa4e5eb0d917fe0701e13edf3
neviliadita17 <61975618+neviliadita17@users.noreply.github.com>
2020-05-02 11:18:57 +0700

    Update poli.blade.php

--- resources/views/pegawai/poli.blade.php
+++ resources/views/pegawai/poli.blade.php
@@ -5,15 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <link rel="stylesheet" href="{{url('/assets/css/admin_style.css')}}">
-    <script src="vue.js"></script>
+    <script src="{{url('/assets/vue/vue.js')}}"></script>
 </head>
 
 <body>
     <div class="topnav" id="myTopnav">
-        <img style="float:left" src="logo.png" width="48px" height="48px">
+        <img style="float:left" src="/img/logo.png" width="48px" height="48px">
         <a style="background-color:inherit; color:inherit;" href="#home">Puskesmas</a>
         <a style="background-color:red; color:inherit; float:right;" class="blogout" href="">
-            <img class="imglogout" src="logout.png">
+            <img class="imglogout" src="/img/logout.png">
             <div class="logout">LOGOUT</div>
         </a>
 
@@ -33,13 +33,13 @@
         <a href="#"><i class="fas fa-file-alt"></i> Manajement
             <hr></a>
     </div>
-
+   
     <div class="main" id="main">
         <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span>
         <div class="header">
             <div class="row">
                 <div class="col-12 col-s-12">
-                    <h1>Poli <span style="float: right;"><a class="button buttonGreen" href="">Tambah Poli</a></span>
+                    <h1>Poli <span style="float: right;"><a class="button buttonGreen" href="{{url('/pegawai/poli/add')}}">Tambah Poli</a></span>
                     </h1>
                 </div>
             </div>
@@ -51,21 +51,21 @@
                     <div>
                         <select v-model="column">
                             <option :value="null">No Column Filter</option>
-                            <option v-for="col in cols" :key="col">{{ col }}</option>
+                            <option v-for="col in cols" :key="col">@{{ col }}</option>
                         </select>
                         <input class="search_bar" v-model="search" type="text" placeholder="Search...">
                     </div>
                     <table class="data_tb">
                         <thead>
                             <tr>
-                                <th v-for="col in cols" :key="col">{{ col }}</th>
+                                <th v-for="col in cols" :key="col">@{{ col }}</th>
                                 <th width="10%">Menu</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr v-for="row in rows" :key="row.id">
-                                <td v-for="col in cols" :key="col">{{ row[col] }}</td>
-                                <td><a class="button buttonBlue" href="">Edit</a></td>
+                                <td v-for="col in cols" :key="col">@{{ row[col] }}</td>
+                                <td><a class="button buttonBlue" href="{{url('/pegawai/poli/edit')}}">Edit</a></td>
                             </tr>
                         </tbody>
                     </table>
