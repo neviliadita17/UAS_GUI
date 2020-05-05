9c93b1e2f7a373a838eee48a5b2b4a637ce2e265
neviliadita17 <61975618+neviliadita17@users.noreply.github.com>
2020-05-05 19:48:33 +0700

    Update home1.blade.php

--- resources/views/pasien/home1.blade.php
+++ resources/views/pasien/home1.blade.php
@@ -5,40 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <link rel="stylesheet" href="{{url('/assets/css/style.css')}}">
-    <script src="vue.js"></script>
+    <script src="{{url('assets/vue/vue.js')}}"></script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 </head>
 
 
 <body>
 
-    <div class="topnav" id="myTopnav">
-        <img style="float:left" src="logo.png" width="48px" height="48px">
-        <a class="atop" href="#" style="background-color:inherit; color:inherit;">Puskesmas</a>
-        <a class="atop" href="#home">Home</a>
-        <a class="atop" href="#pendaftaran">Pendaftaran Antrian</a>
-        <a class="atop" href="#about">Antrian Anda</a>
-        <a class="atop" href="#about">Riwayat Antrian</a>
-        <a class="atop" href="#about">Kontak</a>
-        <a style="background-color:red; color:inherit; float:right;" class="blogout" href="">
-            <img class="imglogout" src="logout.png">
-            <div class="logout">LOGOUT</div>
-        </a>
-        <a href="javascript:void(0);" class="icon" onclick="topNav()">
-            <i class="fa fa-bars"></i>
-        </a>
-    </div>
+    @include('navbar-after')
 
     <div class="main" id="main" id="app">
         <div class="header">
             <div id="home_data" class="container"
-                style="background-image: url(573798-PLOMH2-538.jpg); background-size: cover;">
+                style="background-image: url(/img/573798-PLOMH2-538.jpg); background-size: cover;">
                 <div class="row">
+                @foreach ($tb_pasien as $pasien)
                     <div class="col-12" style="text-align: right;">
                         <h1>Data Pasien</h1>
-                        <h2>Nama Lengkap</h2>
-                        <h2>Nomor BPJS</h2>
-                        <h4>Tanggal Lahir</h4>
-                        <h4>Alamat</h4>
+                        <h2>{{$pasien->nama_pasien}}</h2>
+                        <h2>{{$pasien->n_bpjs}}</h2>
+                        <h4>{{$pasien->tgl_lahir}}</h4>
+                        <h4>{{$pasien->alamat}}</h4>
+                @endforeach
                     </div>
                     <button id="status_btn" type="button">Status</button>
                 </div>
@@ -65,12 +53,12 @@
 
             </div>
             <div class="col-10 col-s-6" id="poli">
-                <div id="home_card" class="card" style="width: 18rem;" v-for="post in filteredList">
-                    <img v-bind:src="post.img" alt="Avatar" style="width:100%">
+                <div id="home_card" class="card" style="width: 18rem;" v-for="row in rows" :key="row.id">
+                    <img v-bind:src="row.gambar_poli" alt="Avatar" style="width:100%">
                     <div class="container" style="overflow-y: auto;">
-                        <h4><b>{{ post.poli }}</b></h4>
-                        <p>{{ post.deskripsi }}</p>
-                        <button type="button">Pilih.</button>
+                        <h4><b>@{{row['nama_poli']}}</b></h4>
+                        <p>@{{row['deskripsi']}}</p>
+                        <button type="button" href="">Lihat Poli</button>
                     </div>
                 </div>
             </div>
@@ -98,59 +86,39 @@
         }
     }
 
-    class Post {
-        constructor(poli, deskripsi, img) {
-            this.poli = poli;
-            this.deskripsi = deskripsi;
-            this.img = img;
-        }
-    }
-
     const app = new Vue({
         el: "#app",
-        data: {
-            search: "",
-            postList: [
-                new Post(
-                    "Poli Umum",
-                    "Poli umum, Berupa pelayanan kesehatan tingkat pertama yaitu Riwayat Jalan Tingkat Pertama(RJTP), meliputi: pemeriksaan, pengobatan, konsultasi medis,tindakan medis non spesialistik, baik operatif maupun non operatif,pelayanan obat dan bahan medis pakai.",
-                    "OGFB4B0.jpg"
-                ),
-                new Post(
-                    "Poli THT",
-                    "Poli THT adalah layanan diagnosa dan terapi berbagai gangguan dan penyakit organ-organ telinga, hidung, dan tenggorokan.",
-                    "OGFB4B0.jpg"
-                ),
-                new Post(
-                    "Poli Kandungan",
-                    "Poli kandungan melayani pemeriksaan kehamilan, penyakit kandungan dan persalinan. Sarana penunjang yang juga kami sediakan untuk mendukung klinik kebidanan dan kandungan ini adalah fasilitas USG untuk mengetahui perkembangan janin pada si ibu hamil.",
-                    "OGFB4B0.jpg"
-                ),
-                new Post(
-                    "Poli Anak",
-                    "Berbagai pelayanan antara lain pemeriksaan kesehatan anak dari bayi baru lahir, balita, hingga menjelang remaja, imunisasi, vaksinasi, konsultasi tumbuh kembang anak di bawah asuhan dokter, konseling ASI, Inisiasi Menyusui Dini (IMD), dll.",
-                    "OGFB4B0.jpg"
-                ),
-                new Post(
-                    "Poli Mata",
-                    "Poli menyediakan berbagai macam layanan pengobatan untuk penanganan kelainan penglihatan dan penyakit seputar mata dengan dukungan peralatan diagnostik mata yang lengkap.",
-                    "OGFB4B0.jpg"
-                ),
-                new Post(
-                    "Poli Gigi",
-                    "Poli Gigi, berupa pelayanan gigi yaitu pemeriksaan, pengobatan, dan konsultasi medis, premedikasi, kegawatdaruratan oro-dental, pencabutan gigi sulung (topical, infiltrasi), pencabutan gigi permanen tanpa penyulit, obat pasca ekstraksi, tumpatan komposit, glass ionomer cement (GIC), scalling (pembersihan karang gigi), serta pelayanan gigi lain yang dapat dilakukan di fasilitas kesehatan tingkat pertama sesuai Panduan Praktik Klinik (PPK) Dokter Gigi.",
-                    "OGFB4B0.jpg"
-                )
-            ]
-        },
+        data: ({
+            search: null,
+            column: null,
+            items: []
+        }),
         computed: {
-            filteredList() {
-                return this.postList.filter((post) => {
-                    return post.poli.toLowerCase().includes(this.search.toLowerCase());
-                });
+            cols() {
+                return this.items.length >= 1 ? Object.keys(this.items[0]) : []
+            },
+            rows() {
+                if (!this.items.length) {
+                    return []
+                }
+                return this.items.filter(item => {
+                    let props = (this.search && this.column) ? [item[this.column]] : Object.values(item)
+                    return props.some(prop => !this.search || ((typeof prop === 'string') ? prop
+                        .includes(this.search) : prop.toString(10).includes(this.search)))
+                })
+            },
+        methods: {
+            upDate: function() {
+                axios.get('http://localhost:8000/pasien/home-api')
+                    .then(response => this.postList = response.data['data'])
             }
+        },
+        mounted() {
+            this.upDate();
+            this.timer = setInterval(this.upDate, 5000)
         }
     });
 </script>
+
 
 </html>
