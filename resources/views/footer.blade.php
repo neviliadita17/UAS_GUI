bb9430d9456f9f99d1b3fe7c14d8f8d0b6c1a5c5
neviliadita17 <61975618+neviliadita17@users.noreply.github.com>
2020-05-09 10:09:57 +0700

    Add files via upload

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="http://localhost:8000/assets/css/admin_style.css">
+    <script src="{{url('/assets/vue/vue.js')}}"></script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+</head>
+
+<body>
+
+        <div class="footer">
+            <footer class="footer-distributed">
+    
+                <div class="footer-left">
+                    <h3>Puskesmas</h3>
+                    <p class="footer-company-name">Puskesmas &copy;copyright2020</p>
+                </div>
+    
+                <div class="footer-right">  
+                    <div>
+                        <i class="fa fa-map-marker"></i>
+                        <p><span>Pendidikan Vokasi</p>
+                        <p><span>Universitas Brawijaya</span> Malang, Jawa Timur</p>
+                    </div>
+            
+                    <div>
+                        <i class="fa fa-phone"></i>
+                        <p>(0341)xxxxx</p>
+                    </div>
+            
+                    <div>
+                        <i class="fa fa-envelope"></i>
+                        <p><a href="">contact@contact.com</a></p>
+                    </div>
+        
+                </div>
+    
+            </div>
+    
+            </footer>
+        </div>
+
+</body>
+
+</html>
